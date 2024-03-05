a434ec914657e6e76390cd69d5551e864f2aa379
EliaRandriantsiory <elia.randriantsiory@gmail.com>
2024-03-05 12:00:20 +0300

    conflit merge

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace todoASPMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult CreationCompte()
+        {
+            return View();
+        }
+    }
+}
