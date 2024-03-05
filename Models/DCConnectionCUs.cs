f58e38e03bc1f17ea0e2ba51bb7801b1ae5af401
Farahary <faratsm@gmail.com>
2024-03-05 16:13:28 +0300

    Create et update

---
+++ Models/DCConnectionCUs.cs
@@ -0,0 +1,62 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace todoASPMVC.Models
+{
+    public class DCConnectionCUs
+    {
+        public static NpgsqlConnection connectionString =
+            new NpgsqlConnection(
+                ConfigurationManager.ConnectionStrings["TPASPNet"].ConnectionString);
+
+
+
+
+        public static void CreationTache(Taches tache)
+        {
+            var req = $"INSERT INTO public.\"taches\"(nomutilisateur, nomtache, statut)" +
+                    $"VALUES('{tache.NomUtilisateur}', '{tache.NomTache}', '{tache.Statut}'); ";
+            try
+            {
+                connectionString.Open();
+                var cmd = new NpgsqlCommand(req, connectionString);
+                cmd.ExecuteNonQuery();
+                connectionString.Close();
+
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static void UpdateTache(Taches tache)
+        {
+            var req = $"UPDATE public.taches SET nomutilisateur = '{tache.NomUtilisateur}', nomtache = '{tache.NomTache}', statut = '{tache.Statut}' WHERE idtache = {tache.IdTache}";
+
+            try
+            {
+                connectionString.Open();
+
+                var cmd = new NpgsqlCommand(req, connectionString);
+                cmd.ExecuteNonQuery();
+
+                connectionString.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+    
+}
